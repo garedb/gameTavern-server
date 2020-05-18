afe59f1b1bd3fadddc417d99de6d17d142e78293
spazevic <spazevic@yahoo.com>
2020-05-18 11:52:06 -0700

    this works

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 let express = require('express')
 let morgan = require('morgan')
 let rowdyLogger = require('rowdy-logger')
+let cors = require('cors')
+let expressJwt = require('express-jwt')
 
 // Instantiate app
 let app = express()
@@ -12,10 +14,11 @@ let rowdyResults = rowdyLogger.begin(app)
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false })) // Accept form data
 app.use(express.json()) // Accept data from fetch (or any AJAX call)
+app.use(cors())
 
 // Routes
 app.use('/auth', require('./controllers/auth'))
-app.use('/profile', require('./controllers/profile'))
+app.use('/profile', expressJwt({ secret: process.env.JWT_SECRET }), require('./controllers/profile'))
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Not Found' })
